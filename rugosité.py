728562053aa05a4c9bbbb1b28590da87da38e610
selbne <seline.benchabane@ensta.fr>
2026-06-04 10:18:54 +0200

    maj bis prcq j'ai fait de la d avec les subplots

--- rugosité.py
+++ rugosité.py
@@ -16,15 +16,14 @@ def rugosite_std(z, size):
     
     #np.reshape(ecarttype, z.shape)
     return ecarttype
-j=0
-for size in taille_fenetre:
+plt.figure(1, figsize=(16, 8))
+for i, size in enumerate(taille_fenetre):
     rugosite = rugosite_std(z, size=size)
     plt.figure(size)
-    plt.subplot(2, 4, j + 1)
+    plt.subplot(2, 4, i + 1)
     plt.imshow(rugosite, cmap='viridis')
     plt.colorbar(label='Rugosité (écart-type local)')
     plt.title(f'Rugosité (écart-type local) avec une fenêtre de {size}x{size}')
-    j+=1
 plt.show()
 
 
@@ -45,8 +44,9 @@ def rugosite_normale(pente, exposition):
     sum_z = convolve(z, mode='constant', cval=0.0)
     r = np.sqrt(np.sum(sum_x)**2 + np.sum(sum_y)**2 + np.sum(sum_z)**2)
     return 1 - r / n
-i=0
-for size in taille_fenetre:
+
+plt.figure(2, figsize=(16, 8))
+for i,size in enumerate(taille_fenetre):
         rugosite = rugosite_normale(pente, exposition, size=size)
         plt.figure(size+1)
         plt.subplot(2, 4, i + 1)
@@ -54,5 +54,4 @@ for size in taille_fenetre:
         plt.legend()
         plt.colorbar(label='Rugosité (dispersion des normales locales)')
         plt.title(f'Rugosité (dispersion des normales locales) avec une fenêtre de {size}x{size}')    
-        i+=1  
 plt.show()
