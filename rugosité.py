7f2837fa211ccb5988560b1967b02409213699f0
selbne <seline.benchabane@ensta.fr>
2026-06-04 10:33:37 +0200

    tracé rugosité

--- rugosité.py
+++ rugosité.py
@@ -2,40 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import convolve, generic_filter, gaussian_filter
 import pente as p
+
 taille_fenetre = [3, 5, 7, 9, 11, 13, 15]
 
-
-#X,Y,z = loader.load_z('txt/reels/bertheaume_z.txt')
 z = np.loadtxt('txt/reels/bertheaume_z.txt')
-#sigma² = E[z²] - E[z]²
 print(z.shape)
-
 
 def rugosite_std(z, size):
     ecarttype = generic_filter(z, np.nanstd, size=size) 
-    
-    #np.reshape(ecarttype, z.shape)
     return ecarttype
-plt.figure(1, figsize=(16, 8))
-for i, size in enumerate(taille_fenetre):
+
+fig1, axes1 = plt.subplots(2, 4, figsize=(16, 8))
+axes1 = axes1.flatten()
+
+for index, size in enumerate(taille_fenetre):
     rugosite = rugosite_std(z, size=size)
-    plt.figure(size)
-    plt.subplot(2, 4, i + 1)
-    plt.imshow(rugosite, cmap='viridis')
-    plt.colorbar(label='Rugosité (écart-type local)')
-    plt.title(f'Rugosité (écart-type local) avec une fenêtre de {size}x{size}')
+    ax = axes1[index]
+    ax.imshow(rugosite, cmap='viridis')
+    ax.set_title(f'Rugosité (écart-type local) avec une fenêtre de {size}x{size}')
+
+axes1[-1].axis('off')
+plt.tight_layout()
 plt.show()
 
-
-
-#dispersion des vecteurs normaux 
-
-#calculs des vecteurs normaux
-
-def rugosite_normale(pente, exposition):
+def rugosite_normale(pente, exposition, size):
     theta = pente
     phi = exposition
-    #ramener ma matrice sous forme de vecteur flatten ou stack 
     x = np.sin(theta) * np.cos(phi)
     y = np.sin(theta) * np.sin(phi)
     z = np.cos(theta)
@@ -45,13 +37,15 @@ def rugosite_normale(pente, exposition):
     r = np.sqrt(np.sum(sum_x)**2 + np.sum(sum_y)**2 + np.sum(sum_z)**2)
     return 1 - r / n
 
-plt.figure(2, figsize=(16, 8))
-for i,size in enumerate(taille_fenetre):
-        rugosite = rugosite_normale(pente, exposition, size=size)
-        plt.figure(size+1)
-        plt.subplot(2, 4, i + 1)
-        plt.imshow(rugosite, cmap='viridis')
-        plt.legend()
-        plt.colorbar(label='Rugosité (dispersion des normales locales)')
-        plt.title(f'Rugosité (dispersion des normales locales) avec une fenêtre de {size}x{size}')    
+fig2, axes2 = plt.subplots(2, 4, figsize=(16, 8))
+axes2 = axes2.flatten()
+
+for index, size in enumerate(taille_fenetre):
+    rugosite = rugosite_normale(pente, exposition, size=size)
+    ax = axes2[index]
+    ax.imshow(rugosite, cmap='viridis')
+    ax.set_title(f'Rugosité (dispersion des normales locales) avec une fenêtre de {size}x{size}')    
+
+axes2[-1].axis('off')
+plt.tight_layout()
 plt.show()
