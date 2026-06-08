5dd001e35666286ecc1a342dc6f5062e2c8f3b31
Poivron123 <bogdan.benlahlou@gmail.com>
2026-06-08 12:16:17 +0200

    quickfix bpi methode

--- attendus_pedago/Methode_Bpi.py
+++ attendus_pedago/Methode_Bpi.py
@@ -14,14 +14,17 @@ import visualisation as vis
 
 
 base = os.path.dirname(os.path.dirname(__file__))  # remonte au dossier PROJETHYDRO
+
 X,Y,Z = xy_loader.load_z(os.path.join(base, 'txt', 'reels', 'Morne_Rouge', 'morneRouge.txt'))
 
 
 G = p.gradient_fcn(Z)
+
 ASP = p._aspect(G[...,0], G[...,1])
 
 BPI = p.bpi_sector_adaptive(Z,ASP, aspect_convention = 'geo_rad', n_bins= 6, angle_width=2*np.pi)
-vis.afficher_2D(X, Y, BPI, title='BPI', Zname='Exposition en radian mais pas randians', cmap='viridis', hillshade=False, niveaux=True, colorbar=True, cotes = True, n_levels=1)
+
+vis.afficher_2D(X, Y, BPI, title='BPI', Zname='BPI qui regarde au bon endroit', cmap='viridis', hillshade=False, niveaux=True, colorbar=True, cotes = True, n_levels=1)
 
 plt.show()
 
